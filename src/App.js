95366f4ba26dac75ed0f25b3e5b10d623f48aaa1
unknown <catalinagonzalez1410@gmail.com>
2021-03-18 11:41:23 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+//------------------------------------------------------------------------------------------
+
+
+
+const App = () => {
+  return (
+    <div>
+      <header className="App-header">
+      <h2>Proyectos</h2>
+      <h2>Sobre mí</h2>
+      <h2>Contactos</h2>
+      </header>
+      <h1>¡Hola! Soy Catalina González</h1>
+      <h4>Front-End Developer</h4>
+    </div>
+  );
+
+}
+export default App;
