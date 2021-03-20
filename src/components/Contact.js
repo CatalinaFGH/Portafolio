e243ddf2956b92c6c14cde017baef9d91d3b0995
unknown <catalinagonzalez1410@gmail.com>
2021-03-20 03:37:20 -0300

    agregados iconos y corregidos los tamaños

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,7 @@
 import './Contact.css'
+import gmail from './gmail.svg'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
 
 const Contact = () => {
     return (
@@ -6,9 +9,14 @@ const Contact = () => {
 
            <div className="contact-title-container">
              <h2 className="contact-title">Contacto</h2>
-             <h3>catalinagonzalez1410@gmail.com</h3>
-             <h3>https://github.com/CatalinaFGH</h3>
-             <h3>https://www.linkedin.com/in/catalina-fgh/</h3>
+           </div>
+           <div className="contact-links-container">
+           
+             <h3><img src={gmail} className="contact-gmail-logo" alt="gmail" />catalinagonzalez1410@gmail.com</h3>
+             
+             <h3><img src={github} className="contact-github-logo" alt="github" />https://github.com/CatalinaFGH</h3>
+             
+             <h3><img src={linkedin} className="contact-linkedin-logo" alt="linkedin" />https://www.linkedin.com/in/catalina-fgh/</h3>
            </div>
 
        </div>
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,21 @@
 import './WelcomePage.css'
 import photo from './photo.png';
+import hand from './hand.svg';
+import laptop from './laptop.svg';
 
 const WelcomePage = () => {
    return (
      <div className="welcome-page-container">
 
       <div className="welcome-page">
-        <h1 className="welcome-page-title">¡Hola!<br></br>Soy Catalina González</h1>
-        <h4 className="welcome-subtitle">Front-End Developer</h4>
+      <img src={photo} className="welcome-page-photo" alt="catalinagonzalez" />
+        <h1 className="welcome-page-title">¡Hola!<img src={hand} className="welcome-page-hand" alt="waving hand" />
+        <br></br>
+        Soy Catalina González</h1>
+        <h4 className="welcome-subtitle">Front-End Developer<img src={laptop} className="welcome-page-laptop" alt="cool laptop" /></h4>
       </div>
 
-      <img src={photo} className="welcome-page-photo" alt="catalinagonzalez" />
+      
 
      </div>
 
