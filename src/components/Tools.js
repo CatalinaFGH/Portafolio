d09e4a212825b90a53859c8888c0213d6b1cb225
unknown <catalinagonzalez1410@gmail.com>
2021-03-22 19:05:34 -0300

    primer intento de responsive

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,5 +1,6 @@
 import './Tools.css'
 import icons from './icons.svg'
+import iconsmobile from './iconsmobile.svg'
 
 const Tools = () => {
     return (
@@ -12,6 +13,7 @@ const Tools = () => {
         
         <div>
           <img src={icons} className="tools-icons" alt="my icons" />
+          <img src={iconsmobile} className="tools-icons-mobile" alt="my icons" />
         </div>
            
        </div>
